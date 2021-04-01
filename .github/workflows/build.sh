ff1e1d02af26cde64f53174b3a116d53eaf6af88
Christopher H. Jordan <christopherjordan87@gmail.com>
2021-04-01 15:20:43 +0800

    Bump version.

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -7,7 +7,6 @@ set -eux
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 
 # Install dependencies
-cd /root
 curl -L "https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio-3.49.tar.gz" -o cfitsio-3.49.tar.gz
 tar -xf cfitsio-3.49.tar.gz
 cd cfitsio-3.49
@@ -17,16 +16,18 @@ cd cfitsio-3.49
 # https://stackoverflow.com/questions/52858556/most-recent-processor-without-support-of-ssse3-instructions
 CFLAGS="-O3" ./configure --prefix="${PWD}" --enable-reentrant --enable-ssse3 --enable-sse2 --disable-curl
 make -j install
-PKG_CONFIG_PATH=/root/cfitsio-3.49
+cd ..
+PKG_CONFIG_PATH=./cfitsio-3.49
 
-cd /root
 curl -L "https://github.com/liberfa/erfa/releases/download/v1.7.2/erfa-1.7.2.tar.gz" -o erfa-1.7.2.tar.gz
 tar -xf erfa-1.7.2.tar.gz
 cd erfa-1.7.2
 CFLAGS="-O3" ./configure --prefix="${PWD}"
 make -j install
-PKG_CONFIG_PATH+=:/root/erfa-1.72
+cd ..
+PKG_CONFIG_PATH+=:./erfa-1.72
+
+export PKG_CONFIG_PATH
 
 # Build
-cd /io
 PKG_CONFIG_ALL_STATIC=1 cargo build --release
